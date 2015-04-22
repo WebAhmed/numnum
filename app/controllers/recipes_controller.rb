1ac67873654d8dfddb1d03fd659ed37c7b87e690
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-04-21 23:05:33 -0700

    add basic basic http auth

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  http_basic_authenticate_with name: "ahmed", password: "nigma0726", except: :index
   before_action :find_recipe, only: [:show,:edit,:update,:destroy]
 
   def index
