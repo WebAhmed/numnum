3622f48cd6c15406019cba91893ed2cced51c3c8
Ahmed Abdelaal <ahmed1500@gmail.com>
2015-04-24 07:42:20 -0700

    add dependency for deletion

--- db/migrate/20150421223037_add_attachment_image_to_recipes.rb
+++ db/migrate/20150421223037_add_attachment_image_to_recipes.rb
@@ -1,11 +1,5 @@
 class AddAttachmentImageToRecipes < ActiveRecord::Migration
-  def self.up
-    change_table :recipes do |t|
-      t.attachment :image
-    end
-  end
-
-  def self.down
-    remove_attachment :recipes, :image
+  def change
+    add_attachment :recipes, :image
   end
 end
